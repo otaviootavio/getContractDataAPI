a53e22289e96c28795f8fbbffb362d0661b36d21
otaviootavio <otaviovacari@usp.br>
2024-03-24 05:18:57 -0300

    verify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,15 +3,37 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 import { config as configEnv } from "dotenv";
 
 configEnv();
+const scrollSepoliaApiKey = process.env.SCROLL_SEPOLIA_API_KEY || "";
 
 const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.24",
+  },
   networks: {
     sepolia: {
       url: "https://scroll-sepolia.public.blastapi.io",
       accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
+    scrollSepolia: {
+      url: "https://sepolia-rpc.scroll.io/",
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
   },
-  solidity: "0.8.24",
+  etherscan: {
+    apiKey: {
+      scrollSepolia: scrollSepoliaApiKey,
+    },
+    customChains: [
+      {
+        network: "scrollSepolia",
+        chainId: 534351,
+        urls: {
+          apiURL: "https://api-sepolia.scrollscan.com/api",
+          browserURL: "https://sepolia.scrollscan.com/",
+        },
+      },
+    ],
+  },
 };
 
 export default config;
